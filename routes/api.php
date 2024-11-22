7cc9f03f0dd6d06d19be914e752943d0707750f5
Edwin Dennis Rivadeneira Salvatierra <e1315400067@live.uleam.edu.ec>
2024-11-22 01:59:12 -0500

    Cambios

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
  
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
- 
+use App\Http\Controllers\DataUsers;
+use PHPUnit\Framework\TestStatus\Risky;
+
 Route::group([
     'middleware' => 'api',
 ], function ($router) {
@@ -11,4 +13,5 @@ Route::group([
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
     Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
     Route::post('/me', [AuthController::class, 'me'])->middleware('auth:api')->name('me');
+    Route::get('/obtenerusuarios', [DataUsers::class, 'obtenerusuarios'])->middleware('auth:api')->name('obtenerusuarios');
 });
